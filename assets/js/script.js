1a9bb823da2538ff754bd244763de6d4748bf5d0
Khaled Silva <khaled.silva17@gmail.com>
2026-05-13 00:13:56 -0300

    feat: add form field validation with error messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,25 @@ btn.forEach(button => {
     document.getElementById(id).classList.remove("hidden");
   });
 });
+
+const form = document.getElementById("c1");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const inputs = form.querySelectorAll(".form-box");
+
+  inputs.forEach(input => {
+
+    const titleErro = input.previousElementSibling;
+    const erro = titleErro.querySelector(".erro");
+
+    if(input.value.trim() === "") {
+      input.classList.add("input-error");
+      erro.classList.add("show-erro");
+    } else {
+      input.classList.remove("input-error");
+      erro.classList.remove("show-erro");
+    }
+  });
+});
